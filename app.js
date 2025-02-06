57d4ba141ce28033a02b4ca9f48dfb50aed901c5
Kevin Lemos <kevinjlemos@hotmail.com>
2025-02-05 23:43:21 -0300

    Captura valor, valida entrada y limpia la caja

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (nombreAmigo === "") {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+
+    limpiarCaja();
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
